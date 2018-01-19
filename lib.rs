dc155cbad18faa579d2953ae1c446412cd63a5c8
Alex Sayers <alex.sayers@gmail.com>
2018-01-19 13:14:02 +0900

    Provide mutex-like semantics when no threads are waiting
    Also, remove the notion of an "emergency" thread.

--- lib.rs
+++ lib.rs
@@ -2,31 +2,25 @@
 A baton encapsulates some state which may be passed around between the threads in a thread pool.
 At any given time, the baton is held by exactly one member of the pool.
 
-We distinguish between two kinds of thread: "regular" threads, of which there may be zero or many,
-and which recieve the baton with high priority; and the "emergency" thread, of which there must be
-exactly one, which will recieve the baton only when none of the regular threads are available to
-take it.
-
 Each thread has a "handle" which it keeps hold of at all times.  This handle is used to take the
 baton.  Don't call `tag_in` on your handle while holding the baton, or else the pool will be
 deadlocked.
-
-The emergency thread must not die or drop its handle. When it's holding the baton, it should call
-`tag_out` regularly in order to attempt to pass it back to a regular thread. Whenver it isn't
-holding the baton it should be blocked on a call to `tag_in`, to prevent the baton from being stuck
-in the emergency thread's inbox.
 */
 
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
-use std::sync::mpsc;
+use std::ptr;
+use std::sync::atomic::{self, AtomicPtr};
+use std::sync::{mpsc, Arc};
 
 /// A baton encapsulates some state which may be passed around between the threads in a thread
 /// pool.
 pub struct Baton<T> {
+    // When releasing, first check to see if there are any waiting threads to hand the baton to...
     queue_rx: mpsc::Receiver<WorkerHandle<T>>,
     heap: BinaryHeap<WorkerHandle<T>>,
-    emergency_tx: mpsc::SyncSender<Baton<T>>,
+    // ...and if not, free the baton.
+    slot: Arc<AtomicPtr<Baton<T>>>,
     pub inner: T,
 }
 
@@ -48,82 +42,85 @@ impl<T> Ord for WithPrio<T> {
 }
 
 /// Allows taking the baton with high priority.
-pub struct RegularHandle<T>(mpsc::Sender<WorkerHandle<T>>);
-impl<T> Clone for RegularHandle<T> {
+pub struct Handle<T> {
+    // When locking, check this first to see if the baton is free...
+    slot: Arc<AtomicPtr<Baton<T>>>,
+    // ...and if not, send a work handle to the active thread.
+    queue_tx: mpsc::Sender<WorkerHandle<T>>,
+}
+impl<T> Clone for Handle<T> {
     fn clone(&self) -> Self {
-        RegularHandle(self.0.clone())
+        Handle {
+            slot: self.slot.clone(),
+            queue_tx: self.queue_tx.clone(),
+        }
     }
 }
 
-/// Allows taking the baton with low priority.
-pub struct EmergencyHandle<T>(mpsc::Receiver<Baton<T>>);
-
-unsafe impl<T: Send> Send for RegularHandle<T> {}
-unsafe impl<T: Send> Send for EmergencyHandle<T> {}
+unsafe impl<T: Send> Send for Handle<T> {}
 
 impl<T> Baton<T> {
     /// Create a new baton.
     ///
-    /// You also get a `RegularHandle`, which may be cloned, and one unclonable `EmergencyHandle`.
-    /// Regular threads may be added to or removed from the pool freely, but you must not drop the
-    /// emergency handle.
-    pub fn new(inner: T) -> (Baton<T>, RegularHandle<T>, EmergencyHandle<T>) {
+    /// You also get a `Handle`, which may be cloned.  Threads may be added to or removed
+    /// from the pool freely.
+    pub fn new(inner: T) -> (Baton<T>, Handle<T>) {
         let (queue_tx, queue_rx) = mpsc::channel();
-        let (emergency_tx, emergency_rx) = mpsc::sync_channel(1);
+        let slot = Arc::new(AtomicPtr::new(ptr::null_mut()));
         let baton = Baton {
             queue_rx: queue_rx,
             heap: BinaryHeap::new(),
-            emergency_tx: emergency_tx,
+            slot: slot.clone(),
             inner: inner,
         };
-        let regular_handle = RegularHandle(queue_tx);
-        let emergency_handle = EmergencyHandle(emergency_rx);
-        (baton, regular_handle, emergency_handle)
+        let handle = Handle {
+            slot: slot,
+            queue_tx: queue_tx,
+        };
+        (baton, handle)
     }
 
     /// Pass the baton to another thread.
     ///
-    /// If any regular threads are ready to take baton (are currently blocked on `tag_in`), then
-    /// one of those will receive it; if not, the emergency thread will receive the baton.
+    /// If any threads are ready to take baton (are currently blocked on `tag_in`), then the one
+    /// which "tagged in" with the highest priority will receive it; if not, the baton will be
+    /// freed.
     ///
     /// This function performs a syscall.  On my machine it takes ~1.7 us.
-    ///
-    /// Note that if all the regular threads are busy, the baton is given to the emergency thread
-    /// whether that thread is ready or not.  This means that if *all* threads are busy when
-    /// `tag_out` is called, and then a regular thread calls `tag_in`, and then the emergency
-    /// thread calls `tag_in`, the baton will be recieved by the emergency thread.
-    pub fn tag_out(mut self) -> Result<(), Error> {
+    pub fn tag_out(mut self) {
         // Drain the queue into the heap
         for x in self.queue_rx.try_iter() {
             self.heap.push(x);
         }
         // And take the max priority element
-        let (tx, is_emg) = match self.heap.pop() {
+        match self.heap.pop() {
             Some(h) => {
-                // Ok, we have another thread waiting to take over. Let's wake it up!
-                (h.inner, false)
+                // Ok, we have another thread waiting to take over. Let's hand it the baton!
+                match h.inner.try_send(self) {
+                    Ok(()) => { /* ok! */ }
+                    Err(mpsc::TrySendError::Disconnected(_)) =>
+                        panic!("Impossible: A thread has put a tx on the queue and dropped the rx.
+                               There must be a bug in Handle::tag_in."),
+                    Err(mpsc::TrySendError::Full(_)) =>
+                        panic!("Impossible: The mvar is already full, but only one baton exists!"),
+                }
             }
             None => {
-                // There are no other threads waiting. Let's use the emergency thread.
-                (self.emergency_tx.clone(), true)
+                // There are no other threads waiting. Let's free the baton!
+                let slot = self.slot.clone();
+                let baton = Box::new(self);
+                let old = slot.compare_and_swap(ptr::null_mut(),
+                                               Box::into_raw(baton),
+                                               atomic::Ordering::Relaxed); // TODO: is Relaxed ok?
+                if old != ptr::null_mut() {
+                    panic!("Impossible: slot wasn't null!");
+                }
             }
-        };
-        match tx.try_send(self) {
-            Ok(()) => Ok(()),
-            Err(mpsc::TrySendError::Disconnected(_)) =>
-                if is_emg {
-                    Err(Error::EmgThreadIsGone)
-                } else {
-                    panic!("Impossible: A thread has put a tx on the queue and dropped the rx.
-                           There must be a bug in RegularHandle::tag_in.")
-                },
-            Err(mpsc::TrySendError::Full(_)) =>
-                panic!("Impossible: The mvar is already full, but only one baton exists!"),
         }
     }
 }
 
-impl<T> RegularHandle<T> {
+impl<T> Handle<T> {
     /// Declare that this thread is ready to receive the baton.
     ///
     /// Blocks until the baton is passed to it.  This function allocates and performs a syscall.
@@ -131,30 +128,28 @@ impl<T> RegularHandle<T> {
     // TODO: Consume self, and return the handle when calling tag_out, in order to prevent
     // deadlocks.
     pub fn tag_in(&self, prio: usize) -> Result<Baton<T>, Error> {
-        let (tx, rx) = mpsc::sync_channel::<Baton<T>>(1);
-        match self.0.send(WithPrio{ prio: prio, inner: tx }) {
-            Ok(()) => {}
-            Err(mpsc::SendError(_)) => return Err(Error::BatonIsGone),
-        }
-        match rx.recv() {
-            Ok(x) => Ok(x),
-            Err(mpsc::RecvError) =>
-                // This case is triggered when (a) the baton is dropped, or (b) the baton-holder
-                // pops tx and then drops it.  (b) is impossible, so it must be (a).
-                Err(Error::BatonIsGone),
-        }
-    }
-}
-
-impl<T> EmergencyHandle<T> {
-    /// Declare that this thread is ready to receive the baton.  Note that this thread will receive
-    /// the baton only as a last resort.
-    ///
-    /// Blocks until the baton is passed to it.  This function allocates and performs a syscall.
-    pub fn tag_in(&self) -> Result<Baton<T>, Error> {
-        match self.0.recv() {
-            Ok(x) => Ok(x),
-            Err(mpsc::RecvError) => Err(Error::BatonIsGone),
+        // First check if the baton is free
+        let x = self.slot.swap(ptr::null_mut(), atomic::Ordering::Relaxed); // TODO: Relaxed?
+        if x != ptr::null_mut() {
+            // It's free!
+            let baton = unsafe { Box::from_raw(x) };
+            Ok(*baton)
+        } else {
+            // The baton is currently held by another thread.
+            // Create an mvar and send it to the active thread.
+            let (tx, rx) = mpsc::sync_channel::<Baton<T>>(1);
+            match self.queue_tx.send(WithPrio{ prio: prio, inner: tx }) {
+                Ok(()) => {}
+                Err(mpsc::SendError(_)) => return Err(Error::BatonIsGone),
+            }
+            // Wait for the mvar to be filled.
+            match rx.recv() {
+                Ok(x) => Ok(x),
+                Err(mpsc::RecvError) =>
+                    // This case is triggered when (a) the baton is dropped, or (b) the baton-holder
+                    // pops tx and then drops it.  (b) is impossible, so it must be (a).
+                    Err(Error::BatonIsGone),
+            }
         }
     }
 }
@@ -163,9 +158,6 @@ impl<T> EmergencyHandle<T> {
 pub enum Error {
     /// Someone has dropped the baton.
     BatonIsGone,
-    /// We tried to pass the baton to the emergency thread, but the emergency handle has been
-    /// dropped.
-    EmgThreadIsGone,
 }
 
 #[cfg(test)]
@@ -176,19 +168,19 @@ mod tests {
 
     #[test]
     fn test_thread_pool() {
-        let (mut baton, reg_h, emg_h) = Baton::new((Instant::now(), vec![]));
+        let (mut baton, h) = Baton::new((Instant::now(), vec![]));
         let mut threads = vec![];
         for thread_num in 1..4 {
-            let h = reg_h.clone();
+            let h = h.clone();
             threads.push(thread::spawn(move||{
                 for i in 0..(10 * thread_num) {
-                    let mut baton = h.tag_in().unwrap();
+                    let mut baton = h.tag_in(thread_num).unwrap();
                     baton.inner.1.push(baton.inner.0.elapsed());
                     thread::sleep(Duration::from_millis(3));
                     let ts = Instant::now();
                     baton.inner.0 = ts;
-                    baton.tag_out().unwrap();
-                    println!("reg {}, iter {:>2}: releasing took {:>5} ns", thread_num, i, ts.elapsed().subsec_nanos());
+                    baton.tag_out();
+                    println!("thread {}, iter {:>2}: releasing took {:>5} ns", thread_num, i, ts.elapsed().subsec_nanos());
                     thread::sleep(Duration::from_millis(5));
                 }
             }));
@@ -197,20 +189,20 @@ mod tests {
 
         // Let's go!
         baton.inner.0 = Instant::now();
-        baton.tag_out().unwrap();
+        baton.tag_out();
         for i in 0..100 {
-            let mut baton = emg_h.tag_in().unwrap();
+            let mut baton = h.tag_in(0).unwrap();
             baton.inner.1.push(baton.inner.0.elapsed());
             thread::sleep(Duration::from_millis(3));
             let ts = Instant::now();
             baton.inner.0 = ts;
-            baton.tag_out().unwrap();
-            println!("emerg, iter {:>2}: releasing took {:>5} ns", i, ts.elapsed().subsec_nanos());
+            baton.tag_out();
+            println!("thread 0, iter {:>2}: releasing took {:>5} ns", i, ts.elapsed().subsec_nanos());
         }
         for t in threads {
             t.join().unwrap();
         }
-        let baton = emg_h.tag_in().unwrap();
+        let baton = h.tag_in(999).unwrap();
         println!("{:?}", baton.inner);
     }
 }
