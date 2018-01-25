ff2273a18eeabf3f6f59aed03707a00c4b404f5a
Alex Sayers <alex.sayers@gmail.com>
2018-01-25 13:26:32 +0900

    simple: Add some tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,6 +3,7 @@ extern crate rand;
 
 use priomutex::*;
 use rand::*;
+use std::sync::Arc;
 use std::thread;
 use std::time::*;
 
@@ -73,6 +74,60 @@ fn test_bench() {
     println!("{:?}", *guard);
 }
 
+#[test]
+fn test_simple_anyone() {
+    let mutex = Arc::new(simple::Mutex::new(Instant::now()));
+    let mut tids = vec![];
+    for n in 1..5 {
+        let mutex = mutex.clone();
+        tids.push(thread::spawn(move||{
+            let mut rng = thread_rng();
+            for _ in 0..10 {
+                let mut x = loop {
+                    if let Some(x) = mutex.try_lock() { break x }
+                    else { thread::yield_now(); }
+                };
+                println!("thread {}: LOCK    {:>5} ns", n, x.elapsed().subsec_nanos());
+                thread::sleep(Duration::from_millis(rng.gen::<u64>() % 32));
+                let ts = Instant::now();
+                *x = ts;
+                x.release_to(None);
+                println!("thread {}: RELEASE {:>5} ns", n, ts.elapsed().subsec_nanos());
+            }
+        }));
+    }
+    for t in tids {
+        t.join().unwrap();
+    }
+    let x = loop { if let Some(x) = mutex.try_lock() { break x } };
+    println!("{:?}", *x);
+}
+
+#[test]
+fn test_simple_myself() {
+    let mutex = Arc::new(simple::Mutex::new(Instant::now()));
+    let mut tids = vec![];
+    for n in 1..5 {
+        let mutex = mutex.clone();
+        tids.push(thread::spawn(move||{
+            let mut rng = thread_rng();
+            for _ in 0..10 {
+                let ts1 = Instant::now();
+                let mut x = loop {
+                    if let Some(x) = mutex.try_lock() { break x }
+                    else { thread::yield_now(); }
+                };
+                println!("thread {}: LOCK    {:>5} ns", n, ts1.elapsed().subsec_nanos());
+                thread::sleep(Duration::from_millis(rng.gen::<u64>() % 32));
+                let ts = Instant::now();
+                x.release_to(Some(thread::current().id()));
+                println!("thread {}: RELEASE {:>5} ns", n, ts.elapsed().subsec_nanos());
+                thread::sleep(Duration::from_millis(rng.gen::<u64>() % 32));
+            }
+        }));
+    }
+    thread::sleep(Duration::from_millis(1000));
+}
 
 /*
 #[test]
