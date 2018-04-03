cfe33db8b2b581ae5269ad341370f1df5557ae37
Alex Sayers <alex.sayers@gmail.com>
2018-04-03 14:51:29 +0900

    Move benchmarks into benches/

--- tests/tests.rs
+++ tests/tests.rs
@@ -9,7 +9,7 @@ use std::thread;
 use std::time::*;
 
 #[test]
-fn foobarqux() {
+fn test_in_order_locking() {
     const N: usize = 10;
 
     let m = Arc::new(Mutex::new(Vec::new()));
@@ -77,35 +77,6 @@ fn test_no_unfair_advantage() {
             panic!("try_lock succeeded when there was a thread waiting!");
         }
     }
-}
-
-#[test]
-fn test_bench() {
-    let mutex = Arc::new(Mutex::new(Instant::now()));
-    let mut tids = vec![];
-    for n in 1..10 {
-        let mutex = mutex.clone();
-        tids.push(thread::spawn(move||{
-            let mut rng = thread_rng();
-            for i in 0..10 {
-                let ts = {
-                    let mut x = mutex.lock(n).unwrap();
-                    println!("thread {}: LOCK    #{:<2} {:>5} ns", n, i, x.elapsed().subsec_nanos());
-                    thread::sleep(Duration::from_millis(rng.gen::<u64>() % 32));
-                    let ts = Instant::now();
-                    *x = ts;
-                    ts
-                };
-                println!("thread {}: RELEASE #{:<2} {:>5} ns", n, i, ts.elapsed().subsec_nanos());
-                thread::sleep(Duration::from_millis(rng.gen::<u64>() % 32));
-            }
-        }));
-    }
-    for t in tids {
-        t.join().unwrap();
-    }
-    let guard = mutex.lock(0).unwrap();
-    println!("{:?}", *guard);
 }
 
 /*
